339b1a0775379d8e3b4111cf4ba665b0a6ae5de1
hongducdev <contact.hongduc@gmail.com>
2024-03-12 17:55:39 +0700

    Update message and skeleton styles

--- src/components/messages/MessageContainer.jsx
+++ src/components/messages/MessageContainer.jsx
@@ -18,7 +18,7 @@ const MessageContainer = () => {
         <>
           <div className="bg-ctp-pink p-4 text-xl rounded-bl-lg mb-2">
             <span className="text-ctp-crust font-semibold">To: </span>
-            <span className="text-ctp-surface0">
+            <span className="text-ctp-crust">
               {selectedConversation.fullName}
             </span>
           </div>
